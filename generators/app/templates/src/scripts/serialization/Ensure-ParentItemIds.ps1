a904e24c07095cd0677adb3006e8012e360f0cb5
Kumar Kirpalani <kumar.kirpalani@versusmind.eu>
2018-11-07 16:27:00 +0100

    Update local zebsite url

--- generators/app/templates/src/scripts/serialization/Ensure-ParentItemIds.ps1
+++ generators/app/templates/src/scripts/serialization/Ensure-ParentItemIds.ps1
@@ -3,7 +3,8 @@ param(
     [string]$siteUrl,
     [string]$password
     )
-    
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 Import-Module SPE
 Write-Host "Processing $serializationRoot for $siteUrl" 
 
@@ -12,22 +13,25 @@ Get-ChildItem $serializationRoot -Recurse |
     ForEach-Object {
         Write-Host "Processing $($_.FullName)" 
         $yml =  Get-Content $_.FullName | Out-String
-        $yml -match  "(?mis)Parent: ""(?<oldParentId>\S+)"".*Path: (?<itemPath>(/\w+)+)"
-        $itemPath = "master:$($matches['itemPath'])" 
-        $oldParentId = $matches['oldParentId'] 
-
-        $parentPath = (Split-Path $itemPath -Parent)
-        $session = New-ScriptSession -Username admin -Password $password -ConnectionUri $siteUrl
-        $newParentId = Invoke-RemoteScript -Session $session -ScriptBlock { (Get-Item -Path  $params.scPath).Id.guid } -Arguments @{ scPath = $parentPath } 
-        Stop-ScriptSession -Session $session
-
-        # Write-Host "`$itemPath : $itemPath"
-        # Write-Host "`$parentPath : $parentPath"
-        # Write-Host "`$oldParentId : $oldParentId"
-        # Write-Host "`$newParentId : $newParentId"
-
-        if ($newParentId) {
-            $yml = $yml -replace "$oldParentId", "$newParentId" 
-            $yml | Out-String | Set-Content -Path $_.FullName
+        if ($yml -match  "(?mis)Parent: ""(?<oldParentId>\S+)"".*Path: (?<itemPath>(/.+)+/SolutionX)"){
+            $itemPath = "master:$($matches['itemPath'])" 
+            $oldParentId = $matches['oldParentId'] 
+    
+            $parentPath = (Split-Path $itemPath -Parent)
+            $session = New-ScriptSession -Username admin -Password $password -ConnectionUri $siteUrl
+            $newParentId = Invoke-RemoteScript -Session $session -ScriptBlock { (Get-Item -Path  "$($params.scPath)").Id.guid } -Arguments @{ scPath = $parentPath } 
+            Stop-ScriptSession -Session $session
+    
+            if ($newParentId -and $oldParentId -ne $newParentId) {
+                Write-Host "`$itemPath : $itemPath"
+                Write-Host "`$parentPath : $parentPath"
+                Write-Host "`$oldParentId : $oldParentId"
+                Write-Host "`$newParentId : $newParentId"
+                
+                $yml = $yml -replace "$oldParentId", "$newParentId" 
+                $yml | Out-String | Set-Content -Path $_.FullName
+            } else{
+                Write-Host "Nothing changed"
+            }
         }
     }
